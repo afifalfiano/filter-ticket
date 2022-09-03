f6edde0ffddf4f466e9bd38455bd56a751cd320a
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-09-03 11:48:31 +0700

    lib: add eslint and prettier for linter

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    semi: 0,
+    'comma-dangle': 0,
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
