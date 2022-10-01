b4e19f5ff6daaf2618faa664e2d5eb14eef21b33
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-10-01 08:26:20 +0700

    fix: rename folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/layouts/Navbar';
+import Navbar from './components/navbar/Navbar';
 import Dashboard from './pages/dashboard/Dashboard';
 import SignIn from './pages/sign-in/SignIn';
 import SignUp from './pages/sign-up/SignUp';
