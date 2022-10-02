42979cb2a6a4fc4e2a988fd5b746f545691419ff
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-10-02 08:39:37 +0700

    fix: validation form signup

--- src/pages/verification_email/VerificationEmail.jsx
+++ src/pages/verification_email/VerificationEmail.jsx
@@ -4,17 +4,17 @@ function VerificationEmail() {
   return (
     <div className="flex h-screen min-h-screen w-screen min-w-screen items-start mt-20 justify-center">
       <div className="text-center">
-        <h1>Verifikasi Email</h1>
+        <h1 className="font-semibold text-lg">Verifikasi Email</h1>
         <div className="my-5 flex justify-center">
           <div className="bg-red-100 w-52 h-52 items-center flex justify-center">
             image
           </div>
         </div>
-        <p className="label w-full">
+        <p className="label w-56">
           Silahkan cek inbox email anda untuk melakukan verifikasi email.
         </p>
-        <div className="form-control mt-5 items-center">
-          <Link className="btn btn-md w-32" to="/sign_in">
+        <div className="form-control mt-5 items-center mx-2">
+          <Link className="btn btn-md btn-block" to="/sign_in">
             Masuk
           </Link>
         </div>
