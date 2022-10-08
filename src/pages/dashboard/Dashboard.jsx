5cbbafd9552adb46e3957c82372fea63a1123820
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-10-08 18:57:05 +0700

    feat: conditional table

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,11 @@
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable jsx-a11y/label-has-associated-control */
+import { useState } from 'react';
 import {
   HiOutlineCloudUpload,
   HiSearch,
@@ -12,12 +15,20 @@ import {
   HiOutlineClipboardCheck,
   HiOutlineClipboardList,
   HiQuestionMarkCircle,
+  HiExclamation,
 } from 'react-icons/hi';
+import { FaUndoAlt } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import styles from './Dashboard.module.css';
 
 function Dashboard() {
+  const [statusData, setStatusData] = useState('open');
   const navigate = useNavigate();
+
+  const handleStatus = (event) => {
+    setStatusData(event.target.value);
+  };
+
   const columns = [
     'No',
     'ID Pelanggan',
@@ -77,13 +88,13 @@ function Dashboard() {
     {
       uuid: '12123',
       id_pelanggan: '3123123',
-      nama_pelanggan: 'putra',
+      nama_pelanggan: 'putrazzz',
       kontak: 'Ardi - 08123123',
       keluhan: 'internet lemot',
       progress: 'router perlu dicek kembali',
       waktu_dibuat: new Date().toLocaleString('id-ID'),
       waktu_diubah: new Date().toLocaleString('id-ID'),
-      status: 'open',
+      status: 'closed',
     },
   ];
   return (
@@ -103,10 +114,13 @@ function Dashboard() {
             <span className="label-text"> Status Keluhan</span>
           </label>
 
-          <select className="select w-full max-w-full input-bordered">
+          <select
+            className="select w-full max-w-full input-bordered"
+            onChange={handleStatus}
+          >
             <option disabled>Pilih Status</option>
-            <option>Open Case</option>
-            <option>Closed Case</option>
+            <option value="open">Open Case</option>
+            <option value="closed">Closed Case</option>
           </select>
         </div>
 
@@ -397,6 +411,42 @@ function Dashboard() {
         </div>
       </div>
 
+      {/* modal delete */}
+      <input type="checkbox" id="my-modal-revert" className="modal-toggle" />
+      <div className="modal">
+        <div className={`${styles['modal-box-custom']}`}>
+          <label
+            htmlFor="my-modal-delete"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 className="text-lg font-bold">Konfirmasi Data Keluhan</h3>
+          <hr className="my-2" />
+
+          <div className="flex flex-col justify-center align-middle items-center">
+            <HiExclamation size={50} color="#FF2E00" />
+
+            <span className="py-4">
+              Apakah anda yakin mengembalikan status data keluhan dari &nbsp;
+              <strong>closed</strong>
+              &nbsp; menjadi &nbsp;
+              <strong>open ?</strong>
+            </span>
+          </div>
+
+          <hr className="my-2 mt-5" />
+          <div className="modal-action justify-center">
+            <label htmlFor="my-modal-revert" className="btn btn-md">
+              Batal
+            </label>
+            <label htmlFor="my-modal-revert" className="btn btn-md btn-error">
+              Kembalikan
+            </label>
+          </div>
+        </div>
+      </div>
+
       <div className="overflow-x-auto mt-8">
         <table className="table w-full">
           <thead>
@@ -406,76 +456,122 @@ function Dashboard() {
               ))}
             </tr>
           </thead>
+          {/* rows.map((item) => {}) */}
           <tbody>
-            {rows.map((item, index) => (
-              <tr className="text-center">
-                <td>{index + 1}</td>
-                <td>{item.id_pelanggan}</td>
-                <td>{item.nama_pelanggan}</td>
-                <td>{item.kontak}</td>
-                <td>{item.keluhan}</td>
-                <td>{item.progress}</td>
-                <td className="text-left">
-                  <p>
-                    Dibuat:
-                    {item.waktu_dibuat}
-                  </p>
-                  <p>
-                    Diubah:
-                    {item.waktu_diubah}
-                  </p>
-                </td>
-                <td>
-                  <span className="badge badge-accent text-white">
-                    {item.status}
-                  </span>
-                </td>
-                <td>
-                  <div className="flex flex-row gap-3 justify-center">
-                    <HiPencil
-                      className="cursor-pointer"
-                      size={20}
-                      color="#D98200"
-                      onClick={() => {
-                        document.getElementById('my-modal-3').click();
-                      }}
-                    />
-                    <HiTrash
-                      size={20}
-                      color="#FF2E00"
-                      className="cursor-pointer"
-                      onClick={() => {
-                        document.getElementById('my-modal-delete').click();
-                      }}
-                    />
-                    <HiEye
-                      size={20}
-                      color="#0D68F1"
-                      className="cursor-pointer"
-                      onClick={() => {
-                        navigate(`/dashboard/detail/${item.uuid}`);
-                      }}
-                    />
-                    <HiOutlineClipboardCheck
-                      size={20}
-                      color="#065F46"
-                      className="cursor-pointer"
-                      onClick={() => {
-                        navigate(`/dashboard/rfo_single/${item.uuid}`);
-                      }}
-                    />
-                    <HiOutlineClipboardList
-                      size={20}
-                      color="#0007A3"
-                      className="cursor-pointer"
-                      onClick={() => {
-                        document.getElementById('my-modal-4').click();
-                      }}
-                    />
-                  </div>
-                </td>
-              </tr>
-            ))}
+            {rows.map((item, index) => {
+              if (item.status === statusData) {
+                return (
+                  <tr className="text-center">
+                    <td>{index + 1}</td>
+                    <td>{item.id_pelanggan}</td>
+                    <td>{item.nama_pelanggan}</td>
+                    <td>{item.kontak}</td>
+                    <td>{item.keluhan}</td>
+                    <td>{item.progress}</td>
+                    <td className="text-left">
+                      <p>
+                        Dibuat:
+                        {item.waktu_dibuat}
+                      </p>
+                      <p>
+                        Diubah:
+                        {item.waktu_diubah}
+                      </p>
+                    </td>
+                    <td>
+                      {item.status === 'open' ? (
+                        <span className="badge badge-accent text-white">
+                          {item.status}
+                        </span>
+                      ) : (
+                        <span className="badge badge-info text-white">
+                          {item.status}
+                        </span>
+                      )}
+                    </td>
+                    <td>
+                      <div className="flex flex-row gap-3 justify-center">
+                        {statusData === 'open' ? (
+                          <>
+                            <HiPencil
+                              className="cursor-pointer"
+                              size={20}
+                              color="#D98200"
+                              onClick={() => {
+                                document.getElementById('my-modal-3').click();
+                              }}
+                            />
+                            <HiTrash
+                              size={20}
+                              color="#FF2E00"
+                              className="cursor-pointer"
+                              onClick={() => {
+                                document
+                                  .getElementById('my-modal-delete')
+                                  .click();
+                              }}
+                            />
+                            <HiEye
+                              size={20}
+                              color="#0D68F1"
+                              className="cursor-pointer"
+                              onClick={() => {
+                                navigate(`/dashboard/detail/${item.uuid}`);
+                              }}
+                            />
+                            <HiOutlineClipboardCheck
+                              size={20}
+                              color="#065F46"
+                              className="cursor-pointer"
+                              onClick={() => {
+                                navigate(`/dashboard/rfo_single/${item.uuid}`);
+                              }}
+                            />
+                            <HiOutlineClipboardList
+                              size={20}
+                              color="#0007A3"
+                              className="cursor-pointer"
+                              onClick={() => {
+                                document.getElementById('my-modal-4').click();
+                              }}
+                            />
+                          </>
+                        ) : (
+                          <>
+                            <FaUndoAlt
+                              size={20}
+                              color="#D98200"
+                              className="cursor-name"
+                              onClick={() => {
+                                document
+                                  .getElementById('my-modal-revert')
+                                  .click();
+                              }}
+                            />
+                            <HiEye
+                              size={20}
+                              color="#0D68F1"
+                              className="cursor-pointer"
+                              onClick={() => {
+                                navigate(`/dashboard/detail/${item.uuid}`);
+                              }}
+                            />
+                            <HiOutlineClipboardCheck
+                              size={20}
+                              color="#065F46"
+                              className="cursor-pointer"
+                              onClick={() => {
+                                navigate(`/dashboard/rfo_single/${item.uuid}`);
+                              }}
+                            />
+                          </>
+                        )}
+                      </div>
+                    </td>
+                  </tr>
+                );
+              }
+            })}
           </tbody>
         </table>
       </div>
