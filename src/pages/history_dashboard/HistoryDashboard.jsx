c065c342b2c558d5f5b3e97770b6c4759c9c9d50
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-10-23 13:24:55 +0700

    fix: filter and search history dasbor

--- src/pages/history_dashboard/HistoryDashboard.jsx
+++ src/pages/history_dashboard/HistoryDashboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable no-nested-ternary */
 /* eslint-disable prefer-template */
 /* eslint-disable react/jsx-one-expression-per-line */
@@ -24,12 +25,14 @@ import {
 } from 'react-icons/hi';
 import { FaUndoAlt } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './HistoryDashboard.module.css';
 import { useAllComplainHistoryMutation } from '../../store/features/complain_history/complainHistoryApiSlice';
-import { setComplainHistory } from '../../store/features/complain_history/complainHistorySlice';
+import { selectAllComplainHistory, setComplainHistory } from '../../store/features/complain_history/complainHistorySlice';
 import ReopenModal from './ReopenModal';
 import { updateBreadcrumb } from '../../store/features/breadcrumb/breadcrumbSlice';
+import { useAllPOPMutation } from '../../store/features/pop/popApiSlice';
+import { setPOP } from '../../store/features/pop/popSlice';
 
 function HistoryDashboard() {
   const columns = [
@@ -45,7 +48,9 @@ function HistoryDashboard() {
     'Aksi',
   ];
   const [showTable, setShowTable] = useState(false);
-  const [pop, setPOP] = useState('all');
+  const [pop, setPOPLocal] = useState('all');
+  const [dataPOP, setdataPOP] = useState([]);
+  const [search, setSearch] = useState('');
   const navigate = useNavigate();
   const [rows, setRows] = useState([]);
   const dispatch = useDispatch();
@@ -66,13 +71,61 @@ function HistoryDashboard() {
     }
   };
 
+  const dataRow = useSelector(selectAllComplainHistory);
+
+  const [allPOP] = useAllPOPMutation();
+
+  const getAllPOP = async () => {
+    try {
+      const data = await allPOP().unwrap();
+      console.log(data, 'ceksaja');
+      if (data.status === 'success') {
+        dispatch(setPOP({ ...data }));
+        console.log('set data', data);
+        setdataPOP(data.data)
+        console.log(dataPOP, 'pp');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onHandleSearch = (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+    console.log(event.target.value.length, 'ooo');
+    setSearch(event.target.value);
+
+    if (event.target.value.length > 0) {
+      const regex = new RegExp(search, 'ig');
+      const searchResult = rows.filter((item) => item.id_pelanggan.match(regex) || item.nama_pelanggan.match(regex) || item.nama_pelapor.match(regex) || item.nomor_pelapor.match(regex));
+      setRows(searchResult);
+    } else {
+      setRows(dataRow.data);
+    }
+  }
+
   useEffect(() => {
     dispatch(updateBreadcrumb([{ path: '/history_dashboard', title: 'Riwayat Dasbor' }]))
+    getAllPOP()
     getAllComplainHistory();
   }, []);
 
   const handlePOP = (event) => {
-    setPOP(event.target.value);
+    console.log(event.target, 'cek');
+    setPOPLocal(event.target.value);
+    console.log(event.target.value, 'how');
+    const dataChanged = dataRow.data.filter((item) => {
+      if (+item.pop_id === +event.target.value) {
+        return item;
+      }
+    })
+    if (event.target.value === 'all') {
+      console.log(dataRow, 'cek gan');
+      setRows(dataRow.data);
+    } else {
+      setRows(dataChanged);
+    }
   };
 
   const getInfo = ($event) => {
@@ -99,9 +152,10 @@ function HistoryDashboard() {
             className="select w-full max-w-full input-bordered"
             onChange={handlePOP}
           >
-            <option disabled>Pilih POP</option>
-            <option value="yogyakarta">Yogyakarta</option>
-            <option value="solo">Solo</option>
+            <option value="all" label="Semua">All</option>
+            {dataPOP?.map((item) => (
+              <option value={item.id_pop} label={item.pop}>{item.pop}</option>
+            ))}
           </select>
         </div>
 
@@ -118,8 +172,9 @@ function HistoryDashboard() {
                 type="text"
                 id="voice-search"
                 className="input input-md input-bordered pl-10 p-2.5 "
-                placeholder="Cari data keluhan..."
-                required
+                placeholder="Cari riwayat data keluhan..."
+                value={search}
+                onChange={onHandleSearch}
               />
             </div>
           </div>
