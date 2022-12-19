39afe9112949537e3b00f36f691e6f225a08d2fc
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-12-19 19:36:31 +0700

    fix: asset check email

--- src/pages/auth/verification_email/VerificationEmail.jsx
+++ src/pages/auth/verification_email/VerificationEmail.jsx
@@ -6,11 +6,12 @@ function VerificationEmail() {
       <div className="text-center">
         <h1 className="font-semibold text-lg">Verifikasi Email</h1>
         <div className="my-5 flex justify-center">
-          <div className="bg-red-100 w-52 h-52 items-center flex justify-center">
+          {/* <div className="bg-red-100 w-52 h-52 items-center flex justify-center">
             image
-          </div>
+          </div> */}
+          <img src="/check_email.svg" alt="https://storyset.com/business" className="w-72 items-center flex justify-center" />
         </div>
-        <p className="label w-56">
+        <p className="label w-72">
           Silahkan cek inbox email anda untuk melakukan verifikasi email.
         </p>
         <div className="form-control mt-5 items-center mx-2">
