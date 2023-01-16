690881c47a6d6ce896b98250532f2dd89e764cf6
Afif Alfiano <m.afif@cakap.id>
2023-01-16 20:25:48 +0700

    responsive: rfo keluhan

--- src/pages/reason_of_outage/detail_mandiri/RFODetailSingle.jsx
+++ src/pages/reason_of_outage/detail_mandiri/RFODetailSingle.jsx
@@ -96,27 +96,27 @@ function RFODetailSingle() {
   const navigate = useNavigate();
   return (
     <>
-      <div className="w-full py-5 px-5 flex w-min-full bg-blue-200 rounded-md">
+      <div className="w-full py-5 px-5 flex-col gap-3 lg:flex-row md:gap-0 flex w-min-full bg-blue-200 rounded-md">
         <div className="flex-1 w-full">
           <table className="border-none items-center w-full">
             <tbody>
               <tr className="text-left">
-                <td>Referensi Keluhan</td>
+                <td className="w-36 md:w-52 lg:w-auto">Referensi Keluhan</td>
                 <td>:</td>
                 <td>{detailRFO?.keluhan?.nomor_keluhan}</td>
               </tr>
               <tr className="text-left">
-                <td>Pelanggan</td>
+                <td className="w-36 md:w-52 lg:w-auto">Pelanggan</td>
                 <td>:</td>
                 <td>{detailRFO?.keluhan?.id_pelanggan} - {detailRFO?.keluhan?.nama_pelanggan}</td>
               </tr>
               <tr className="text-left">
-                <td>Kontak</td>
+                <td className="w-36 md:w-52 lg:w-auto">Kontak</td>
                 <td>:</td>
                 <td>{detailRFO?.keluhan?.nama_pelapor} - {detailRFO?.keluhan?.nomor_pelapor}</td>
               </tr>
               <tr className="text-left">
-                <td>Sumber Keluhan</td>
+                <td className="w-36 md:w-52 lg:w-auto">Sumber Keluhan</td>
                 <td>:</td>
                 <td>{detailRFO?.keluhan?.sumber?.sumber}</td>
               </tr>
@@ -127,12 +127,12 @@ function RFODetailSingle() {
           <table className="border-none items-center w-full">
             <tbody>
               <tr className="text-left">
-                <td>Waktu Dibuat</td>
+                <td className="w-36 md:w-52 lg:w-auto">Waktu Dibuat</td>
                 <td>:</td>
                 <td>{new Date(detailRFO?.keluhan?.created_at).toLocaleString('id-ID')}</td>
               </tr>
               <tr className="text-left">
-                <td>Waktu Diubah</td>
+                <td className="w-36 md:w-52 lg:w-auto">Waktu Diubah</td>
                 <td>:</td>
                 <td>
                   {detailRFO?.keluhan?.balasan?.length > 0
@@ -141,12 +141,12 @@ function RFODetailSingle() {
                 </td>
               </tr>
               <tr className="text-left">
-                <td>Status Keluhan</td>
+                <td className="w-36 md:w-52 lg:w-auto">Status Keluhan</td>
                 <td>:</td>
                 <td>{detailRFO?.keluhan?.status}</td>
               </tr>
               <tr className="text-left">
-                <td>Detail Sumber Keluhan</td>
+                <td className="w-36 md:w-52 lg:w-auto">Detail Sumber Keluhan</td>
                 <td>:</td>
                 <td>{detailRFO?.keluhan?.detail_sumber}</td>
               </tr>
@@ -154,7 +154,7 @@ function RFODetailSingle() {
           </table>
         </div>
       </div>
-      <div className="flex w-full gap-5 mt-5">
+      <div className="flex w-full gap-5 mt-5 flex-col-reverse lg:flex-row">
         <div className="flex-1 w-full mt-5">
           <h1 className="text-center font-semibold">Reason For Outage Keluhan</h1>
 
@@ -248,7 +248,7 @@ function RFODetailSingle() {
                     ) : null}
                   </div>
 
-                  <div className="flex gap-5">
+                  <div className="flex flex-col lg:flex-row gap-5">
                     <div className="form-control flex-1">
                       <label htmlFor="mulai_gangguan" className="label">
                         <span className="label-text"> Waktu Mulai Keluhan</span>
