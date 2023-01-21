26ee89b107a77299df8a0f5e63606932f6ece05c
Afif Alfiano <m.afif@cakap.id>
2023-01-21 19:47:10 +0700

    remove: src

--- src/components/common/Container.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Container({ children }) {
-  return <div className="p-6">{children}</div>;
-}
-
-export default Container;
--- src/components/common/forms/Required.jsx
+++
@@ -1,9 +0,0 @@
-function Required({...props}) {
-    return (
-        <div className="label text-red-500 pb-0 text-xs">
-            {props.errors}
-        </div>
-    )
-}
-
-export default Required;
